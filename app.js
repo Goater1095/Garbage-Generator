fb3fcbd47491ed4972e8f7818d5d7de3a7154db5
goater <goater1095@gmail.com>
2020-10-26 23:03:43 +0800

    feat: modify README & add clickOn keeping function

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 // set related express module
-const express = require('express')
-const app = express()
-const port = 3000
-const generateTrash = require('./generator')
+const express = require('express');
+const bodyParser = require('body-parser');
+const generateTrash = require('./generator');
 
+const app = express();
+const port = 3000;
 
-//set related express-handlebars 
-const exhbs = require('express-handlebars')
-app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+//set body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//set related express-handlebars
+const exhbs = require('express-handlebars');
+app.engine('handlebars', exhbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // set routes
 app.get('/', (req, res) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
-app.get('/produce', (req, res) => {
-  const optionCareer = req.query.career
-  const trashTalk = generateTrash(optionCareer)
-  res.render('index', { trashTalk })
-})
+app.post('/produce', (req, res) => {
+  const optionCareer = req.body.career;
+  let engineerOn = 0;
+  let designerOn = 0;
+  let entrepreneurOn = 0;
+  if (optionCareer === 'engineer') {
+    engineerOn = 1;
+  } else if (optionCareer === 'designer') {
+    designerOn = 1;
+  } else if (optionCareer === 'entrepreneur') {
+    entrepreneurOn = 1;
+  }
+
+  const trashTalk = generateTrash(optionCareer);
+  res.render('index', { trashTalk, engineerOn, designerOn, entrepreneurOn });
+});
 
 // listen & start up the server
 app.listen(port, () => {
-  console.log(`This Server start on http://localhost:${port}`)
-})
+  console.log(`This Server start on http://localhost:${port}`);
+});
